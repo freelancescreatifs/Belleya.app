e0043e90a48e6e49cd3bb7572613d9a61bbc55af
freelancescreatifs <freelances.creatifs@gmail.com>
2026-03-03 18:02:02 +0100

    Updated SubscriptionStatus.tsx

--- src/components/settings/SubscriptionStatus.tsx
+++ src/components/settings/SubscriptionStatus.tsx
@@ -29,7 +29,7 @@ export default function SubscriptionStatus() {
         .eq('company_id', companyId)
         .order('created_at', { ascending: false })
         .limit(1)
-        .single();
+        .maybeSingle();
 
       setSubscriptionData(data);
     } catch (error) {
